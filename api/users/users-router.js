ddde7e2d86075203b4a2d59161295b7975bed9c0
Xaver Pinero <xaver.pinero@gmail.com>
2021-09-05 22:07:52 -0400

    wip

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,6 +1,6 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 const router = require('express').Router()
-
+const { restricted } = require('../auth/auth-middleware')
 
 
 /**
@@ -25,7 +25,7 @@ const router = require('express').Router()
     "message": "You shall not pass!"
   }
  */
-router.get('/', (req, res, next) => { // eslint-disable-line
+router.get('/', restricted, (req, res, next) => { // eslint-disable-line
   res.json('users')
 })
 
